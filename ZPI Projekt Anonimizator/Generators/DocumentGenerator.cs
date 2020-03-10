39740260a7ed4cf926cc535fd01f1c970f188503
Sheygra <sheygra@gmail.com>
2020-03-10 13:03:10 +0100

    Interfaces for Parsers and Generators

---
+++ ZPI Projekt Anonimizator/Generators/DocumentGenerator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZPI_Projekt_Anonimizator.Generators
+{
+    interface DocumentGenerator
+    {
+        public String generateDocument(String patientData); // bierze dane pacjenta, generuje dokument i zwraca sciezke do niego
+        
+    }
+}
---
+++ ZPI Projekt Anonimizator/Parsers/DocumentParser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace ZPI_Projekt_Anonimizator.Parsers
+{
+    interface DocumentParser
+    {
+        public DataRow parseDocument(String path); //bierze sciezke do dokumentu i zwraca dane w nim zawarte w formie rekordu tabeli,
+                                                   //ktory mozna potem wstawic do DataTable, ktore bedzie anonimizowane
+    }
+}
