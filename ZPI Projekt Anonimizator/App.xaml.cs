49da14bf9384fbbff98b9fec5cac098a1b19dbff
Sheygra <sheygra@gmail.com>
2020-03-10 12:54:29 +0100

    Initial commit

---
+++ ZPI Projekt Anonimizator/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ZPI_Projekt_Anonimizator
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
