a16b91156a924a45e86843c9e326171576345f87
Sheygra <sheygra@gmail.com>
2020-03-19 18:48:24 +0100

    JPG generator refactor

--- ZPI Projekt Anonimizator/Generators/JPGGenerator.cs
+++ ZPI Projekt Anonimizator/Generators/JPGGenerator.cs
@@ -14,45 +14,42 @@ namespace ZPI_Projekt_Anonimizator.Generators
         public String generateDocument(String patientData)
         {
             String resource_file_path = resource_dir_path + filename;
+            String new_file_path = resource_dir_path + "new_img.jpg";
             FileInfo originalImage = new FileInfo(resource_dir_path);
-
-            BitmapDecoder decoder = null;
-            BitmapFrame bitmapFrame = null;
-            BitmapMetadata metadata = null;
+            
             try
             {
                 if (File.Exists(resource_file_path))
                 {
-                    using (Stream jpegStreamIn = File.Open(resource_file_path, FileMode.Open, FileAccess.ReadWrite, FileShare.None))
+                    File.Copy(resource_file_path, new_file_path, true);
+                    BitmapDecoder decoder = null;
+                    using (Stream new_file_stream = File.Open(new_file_path, FileMode.Open, FileAccess.ReadWrite, FileShare.None))
                     {
-
-                        decoder = new JpegBitmapDecoder(jpegStreamIn, BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.OnLoad);
+                         decoder = new JpegBitmapDecoder(new_file_stream, BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.OnLoad);
                     }
-
-                    bitmapFrame = decoder.Frames[0];
-
+                    BitmapFrame bitmapFrame = decoder.Frames[0];
+                    
                     if (bitmapFrame != null)
                     {
                         BitmapMetadata meta_Data = (BitmapMetadata)bitmapFrame.Metadata.Clone(); //metadane przykładowego obrazu 
 
                         if (meta_Data != null)
-                        { 
+                        {
                             meta_Data.Comment = "NEW COMMENT";
-                            
+
                             JpegBitmapEncoder encoder = new JpegBitmapEncoder();
                             encoder.Frames.Add(BitmapFrame.Create(bitmapFrame, bitmapFrame.Thumbnail, meta_Data, bitmapFrame.ColorContexts));
-
-                            using (Stream jpegStreamOut = File.Open(resource_dir_path + "new_img.jpg", FileMode.CreateNew, FileAccess.ReadWrite))
+                            using (Stream new_file_stream = File.Open(new_file_path, FileMode.Open, FileAccess.ReadWrite, FileShare.None))
                             {
-                                encoder.Save(jpegStreamOut);
+                                new_file_stream.Flush();
+                                encoder.Save(new_file_stream);
                             }
-                        
-                        } 
+                        }
                     }
-
-                    return "EXISTS";
+                    
+                    return new_file_path;
                 }
-                else return "DOESNT EXIST";
+                else return "RESOURSE FILE DOESNT EXIST";
             }
             catch(Exception e)
             {
