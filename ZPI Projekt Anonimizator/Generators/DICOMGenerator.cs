307068184e330657ec9cb367ce45c18d40dd62c0
i-jarocki <i.jarocki72@gmail.com>
2020-04-03 15:23:36 +0200

    Added DICOM Generator and Parser

---
+++ ZPI Projekt Anonimizator/Generators/DICOMGenerator.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Dicom;
+
+namespace ZPI_Projekt_Anonimizator.Generators
+{
+    class DICOMGenerator : DocumentGenerator
+    {
+        private String resource_dir_path = @"C:\Users\ASUS UX410U\source\repos\ZPI\ZPI Projekt Anonimizator\resource";
+        private Random gen = new Random();
+        private DateTime date_start = new DateTime(1920, 1, 1);
+
+        public String generateDocument(String patientData)
+        {
+            String resource_file_path = resource_dir_path + @"\MRBRAIN.DCM";
+            String new_file_path = resource_dir_path + generateNewFileName();
+
+            try
+            {
+                var source_file = DicomFile.Open(resource_file_path);
+                var output_file = source_file.Clone();
+                output_file.Dataset.AddOrUpdate(DicomTag.PatientName, "Artur");
+                output_file.Dataset.AddOrUpdate(DicomTag.PatientBirthDate, RandomDay().ToString());
+                output_file.Dataset.AddOrUpdate(DicomTag.PatientSex, RandomSex());
+                output_file.Dataset.AddOrUpdate(DicomTag.PatientWeight, gen.Next(200));
+                output_file.Save(new_file_path);
+
+                return new_file_path;
+            }
+            catch (Exception e)
+            {
+                return "EXCEPTION CAUGHT:" + e;
+            }
+        }
+        private String generateNewFileName()
+        {
+            Random r = new Random();
+            string s = "DicomFile_";
+
+            return "new_file_" + r.Next(1, 1000000) + s[r.Next(0, s.Length - 1)] + ".DCM";
+        }
+        private DateTime RandomDay()
+        {
+            int range = (DateTime.Today - date_start).Days;
+            return date_start.AddDays(gen.Next(range));
+        }
+
+        private String RandomSex()
+        {
+            String sex = "";
+            int number = gen.Next(3);
+            if (number == 0)
+            {
+                sex = "M";
+            }
+            else if (number == 1)
+            {
+                sex = "F";
+            }
+            return sex;
+        }
+    }
+}
---
+++ ZPI Projekt Anonimizator/Parsers/DICOMParser.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Text;
+using Dicom;
+
+namespace ZPI_Projekt_Anonimizator.Parsers
+{
+    class DICOMParser : DocumentParser
+    {
+        public DataTable parseDocument(String dir_path)
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("Name", typeof(String));
+            table.Columns.Add("BirthDate", typeof(String));
+            table.Columns.Add("Sex", typeof(String));
+            table.Columns.Add("Weight", typeof(String));
+
+            try
+            {
+                string[] files = Directory.GetFiles(dir_path, "*.dcm");
+
+                foreach (String file_path in files)
+                {
+                    var file = DicomFile.Open(file_path);
+                    var row = table.NewRow();
+
+                    row["Name"] = file.Dataset.GetSingleValue<string>(DicomTag.PatientName);
+                    row["BirthDate"] = file.Dataset.GetSingleValue<string>(DicomTag.PatientBirthDate);
+                    row["Sex"] = file.Dataset.GetSingleValue<string>(DicomTag.PatientSex);
+                    row["Weight"] = file.Dataset.GetSingleValue<string>(DicomTag.PatientWeight);
+
+                    table.Rows.Add(row);
+                }
+
+                return table;
+            }
+            catch (Exception e)
+            {
+                var row = table.NewRow();
+                row["Comment"] = e.ToString();
+                table.Rows.Add(row);
+                return table;
+            }
+        }
+    }
+}
